fe5de4fa75153458d68462d616344ea309a73fd2
neronix17 <39186138+neronix17@users.noreply.github.com>
2019-02-15 07:25:11 +0000

    Biel Tan
    - Biel Tan Faction.
    - Many edits.
    - Blackstone removed from rock tables.
    - Gauss desert split into 3.
    - Housekeeping

---
+++ Source/Rimhammer40k/BiomeWorker_GaussDesertGreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace Rimhammer40k
+{
+    public class BiomeWorker_GaussDesertGreen : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+            float result;
+            if (tile.WaterCovered)
+            {
+                result = -100f;
+            }
+            else if (tile.temperature < 34f)
+            {
+                result = 0f;
+            }
+            else if (tile.rainfall < 600f || tile.rainfall >= 2000f)
+            {
+                result = 0f;
+            }
+            else
+            {
+                result = 22.5f + tile.temperature * 1.8f + (tile.rainfall - 600f) / 85f;
+            }
+            return result;
+        }
+    }
+}
--- Source/SparklingWorlds/MechanicalPawn.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-namespace Rimhammer40k
-{
-    public class MechanicalPawn : Pawn
-    {
-
-    }
-}
