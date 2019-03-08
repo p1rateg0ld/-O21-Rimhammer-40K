3549f764f46aeb39176be931b1fc24c2723224da
neronix17 <39186138+neronix17@users.noreply.github.com>
2019-03-08 14:49:08 +0000

    Craptop Commit

---
+++ Source/Rimhammer40k/Rimhammer40kController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+using HugsLib;
+
+namespace Rimhammer40k
+{
+    [EarlyInit]
+    public class Rimhammer40kController : ModBase
+    {
+        public override string ModIdentifier
+        {
+            get { return "Rimhammer40k"; }
+        }
+
+        public override void EarlyInitalize()
+        {
+            Logger.Message(":: Rimhammer40k Active ::");
+        }
+    }
+}
