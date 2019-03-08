3d6eb574fc0214953902559a0141fa8a8c653706
neronix17 <39186138+neronix17@users.noreply.github.com>
2019-03-08 16:01:35 +0000

    Merge Commit

---
+++ Source/Rimhammer40k/Orks/CompProperties_OrkSpore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace Rimhammer40k.Orks
+{
+    public class CompProperties_OrkSpore : CompProperties
+    {
+
+    }
+}
