c258db2331c15576c215a48d6fc4abe4645e92b5
neronix17 <39186138+neronix17@users.noreply.github.com>
2019-03-18 19:37:57 +0000

    Pre-Update Jitters
    Fuck you and fuck your changelog

--- Source/Rimhammer40k/Necrons/PawnKindDef.cs
+++ Source/Rimhammer40k/Necrons/PawnKindDef.cs
@@ -7,29 +7,31 @@ namespace Rimhammer40k.Necrons
     public static class PawnKindDefOf
     {
         public static PawnKindDef NecronWarrior;
-        //public static PawnKindDef NecronWarriorOrange;
-        //public static PawnKindDef NecronWarriorBlue;
+        public static PawnKindDef NecronWarriorOrange;
+        public static PawnKindDef NecronWarriorBlue;
 
         //public static PawnKindDef NecronImmortal;
         //public static PawnKindDef NecronImmortalOrange;
         //public static PawnKindDef NecronImmortalBlue;
 
         public static PawnKindDef NecronDeathmark;
-        //public static PawnKindDef NecronDeathmarkOrange;
-        //public static PawnKindDef NecronDeathmarkBlue;
+        public static PawnKindDef NecronDeathmarkOrange;
+        public static PawnKindDef NecronDeathmarkBlue;
 
         public static PawnKindDef NecronLord;
-        //public static PawnKindDef NecronLordOrange;
-        //public static PawnKindDef NecronLordBlue;
+        public static PawnKindDef NecronLordOrange;
+        public static PawnKindDef NecronLordBlue;
 
         //public static PawnKindDef NecronCryptek;
         //public static PawnKindDef NecronCryptekOrange;
         //public static PawnKindDef NecronCryptekBlue;
 
         public static PawnKindDef NecronOverlord;
-        //public static PawnKindDef NecronOverlordOrange;
-        //public static PawnKindDef NecronOverlordBlue;
+        public static PawnKindDef NecronOverlordOrange;
+        public static PawnKindDef NecronOverlordBlue;
 
-        public static PawnKindDef CanoptekScarab;
+        public static PawnKindDef NecronPariah;
+        public static PawnKindDef NecronPariahOrange;
+        public static PawnKindDef NecronPariahBlue;
     }
 }
